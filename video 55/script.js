8150a6ce09116bcbe22e779155837c60c7420a0e
Abbujaa <113247506+Abbujaa@users.noreply.github.com>
2024-10-08 18:09:16 +0530

    Add files via upload

---
+++ video 55/script.js
@@ -0,0 +1,44 @@
+console.log("hey this is tutorial js")
+
+let a = 5;
+let b = 10;
+let c = "harry";
+let _a="kela";
+// var 55a= "rohanta"; ??not allowed
+
+
+// console.log(a + b);
+// console.log(typeof a, typeof b, typeof c);
+
+{
+    let a = 66;
+    console.log(a)
+}
+
+console.log(a)
+
+let x = "harry bhai";
+let y = 22;
+let z = 3.55;
+const p = true ;
+let q = undefined;
+let r = null;
+
+console.log(x,y,z,p,q,r);
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+
+let o ={
+    name: "harry",
+    "job code": 5600
+}
+
+console.log(o)
+o.salary="500crores"
+console.log(o)
+
+
+// let is block scope 
+// var is global scope
+// const a1=6;
+// a1=a1+1; // not allowed as a is constant
